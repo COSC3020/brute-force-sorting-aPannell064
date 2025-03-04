18b1fef308e89e4cdfb72acec4f05983c5f2db65
aPannell064 <adamkpannell@gmail.com>
2025-03-04 11:25:05 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 
 function permutationSort(a) {
     var count = 1;
+    
     var n = a.length
     var c = new Array(n).fill(0);
     var i = 1;
@@ -8,15 +9,18 @@ function permutationSort(a) {
     while(i < n) {
         if(isSorted(a)) {return count;}
 
-        if(i%2 == 0) {[a[0], a[i]] = [a[i], a[0]];}
-        else {[a[c[i]], a[i]] = [a[i], a[c[i]]];}
+        //if(i%2 == 0) {
+        [a[0], a[i]] = [a[i], a[0]];
+        //}
+        //else {[a[c[i]], a[i]] = [a[i], a[c[i]]];}
 
         count++;
         c[i]++;
         i=1;
-
-        for(var j = i; c[j] >= j; j++) {c[i++] = 0;}
+        console.log(a)
+        for(var j = i; c[j] >= j; j++) {c[i++] = 0; }
     }
+    
     return count; 
 
 }
@@ -27,3 +31,17 @@ function isSorted(a) {
     }
     return true;
 }
+
+console.log(permutationSort([1,2,4,3]))
+//console.log(permutationSort([2,1,3]))
+//console.log(permutationSort([3,1,2]))
+//console.log(permutationSort([1,3,2]))
+//console.log(permutationSort([2,3,1]))
+//console.log(permutationSort([3,2,1]))
+
+/*
+This implementation works by finding all permuations found by between just the first two elements. 
+Once all of those permutations are found (there is only two at this level so only one swap needs to be made)
+the 
+
+*/
